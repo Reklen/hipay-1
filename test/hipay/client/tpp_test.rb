a7756a122701acc4c47eb8a293d5d2625e1ab4a0
Arpsara <arpsara@ct2c.fr>
2017-12-01 16:21:01 +0100

    Test post_v1_order_success (pending)

--- test/hipay/client/tpp_test.rb
+++ test/hipay/client/tpp_test.rb
@@ -27,12 +27,39 @@ class TPP < Test::Unit::TestCase
         currency: "EUR"
       })
 
-      puts response
-
       assert_not_nil response
       assert_equal response['order']['id'], "TEST-HPAYMENT-001"
       assert_equal response['order']['amount'], "74.12"
     end
   end
+
+  def post_v1_order_success
+    VCR.use_cassette("post_v1_order_success") do
+      response = @hipay.post('v1/order', {
+        orderid: "TEST-ORDER-001",
+        operation: "Authorization",
+        payment_product: "cb",
+        description: "Desc test",
+        amount: "72.13",
+        accept_url: "http://test.com",
+        decline_url: "http://test.com",
+        pending_url: "http//test.com",
+        exception_url: "http://test.com",
+        cancel_url: "http://test.com",
+        firstname: "Jane",
+        lastname: "Doe",
+        currency: "EUR"
+      })
+
+      # PENDING TEST - RESPONSE
+      # {"code"=>"1020001", "message"=>"No route to acquirer", "description"=>nil}
+
+      assert_not_nil response
+      pending assert_equal response['order']['id'], "TEST-ORDER-001"
+      pending assert_equal response['order']['amount'], "72.13"
+    end
+  end
+
+
 
 end
